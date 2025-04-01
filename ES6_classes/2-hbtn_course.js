9eb035f3b1eb7e952748ce6fc19eb0bd02e3301d
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-01 16:03:19 +1100

    fix: 2-hbtn_course.js throw new error

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -15,7 +15,7 @@ export default class HolbertonCourse {
     if (typeof name === 'string') {
       this._name = name;
     } else {
-      throw TypeError('Name must be a string');
+      throw new TypeError('Name must be a string');
     }
   }
 
@@ -23,7 +23,7 @@ export default class HolbertonCourse {
     if (typeof length === 'number') {
       this._length = length;
     } else {
-      throw TypeError('length must be a number');
+      throw new TypeError('length must be a number');
     }
   }
 
