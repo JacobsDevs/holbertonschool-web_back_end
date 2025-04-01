f7ec80b6f569522f17740043cb78d6dc3921bdd1
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-01 16:44:37 +1100

    add: 4-pricing.js

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('Currency must be a Currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    }
+    throw new TypeError('Amount and Conversion Rate must be number');
+  }
+}
