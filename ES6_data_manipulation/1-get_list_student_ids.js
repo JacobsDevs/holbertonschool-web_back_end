4ac89d1afc905b40e623bfb31e0aa82183fc3875
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-02 14:40:33 +1100

    add: 1-get_list_student_ids.js

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,7 @@
 export default function getListStudentIds(arr) {
   if (typeof arr === 'object') {
-    return arr.map((x) => x.id);
+    const res = arr.map((x) => x.id);
+    return res;
   }
   return [];
 }
