40c8d9d0edc4d71ef14f2c11cf2c8fbd4c1d4760
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-02 16:11:04 +1100

    add: 4-update_grade_by_city.ks

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,15 @@
 export default function updateStudentGradeByCity(arr, city, newGrades) {
   if (arr instanceof Array && typeof city === 'string' && newGrades instanceof Array) {
-    const res = arr.filter((x) => x.location === city);
-    const final = res.map((x) => ({ ...x, grade: newGrades.filter((y) => y.id === x.id).grade }));
-    return final;
+    return arr.filter((x) => x.location === city).map((y) => {
+      let s = newGrades.filter((z) => z.studentId === y.id).reduce((acc, i) => acc + i.grade, 0);
+      if (s === 0) {
+        s = 'N/A';
+      }
+      return {
+        ...y,
+        grade: s,
+      };
+    });
   }
   return 0;
 }
