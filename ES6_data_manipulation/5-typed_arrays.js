2ba638e494b9eaf0af5d5bddc2955829d8baf18e
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-03 09:28:19 +1100

    fix: 5-typed_arrays.js to throw RangeError

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) {
-    return 'Position outside range';
+  if (position >= length || position < 0) {
+    throw new RangeError('Position outside range');
   }
   const buff = new ArrayBuffer(length);
   const view = new DataView(buff);
