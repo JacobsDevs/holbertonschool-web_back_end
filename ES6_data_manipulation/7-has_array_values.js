f9fe7b6c32ad334cb50f50a7cdf662e3a7f86dc5
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-03 09:53:07 +1100

    fix: 7-* lint

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -2,4 +2,5 @@ export default function hasValuesFromArray(set, array) {
   if (set instanceof Set && array instanceof Array) {
     return array.every((x) => set.has(x));
   }
+  throw new TypeError('array must be array and set must be set');
 }
