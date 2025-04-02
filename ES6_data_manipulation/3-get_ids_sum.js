de58cdc4a7f578881c79ec59900f9c5e682f3fa5
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-02 14:58:15 +1100

    fix: 3-* to not have 2-* parameters

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,7 @@
-export default function getListStudentIds(arr, city) {
-  if (arr instanceof Array && typeof city === 'string') {
+export default function getListStudentIds(arr) {
+  if (arr instanceof Array) {
     const res = arr.reduce((acc, x) => acc + x.id, 0);
     return res;
   }
-  return [];
+  return 0;
 }
