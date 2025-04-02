3690965b38e15b260a08889ef5027c8428e4f944
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-03 09:27:05 +1100

    fix: 5-typed_arrays.js to return string error

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    return 'Position outside range';
+  }
   const buff = new ArrayBuffer(length);
   const view = new DataView(buff);
   view.setUint8(position, value);
