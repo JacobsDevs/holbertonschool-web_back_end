dd2dcb3f7f015ab655fbd8e1793d751ff75e9081
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-03 14:30:36 +1100

    fix: 3-all.js test finally

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils.js';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then()
-    .finally(console.log(body, firstName, lastName));
+    .finally(console.log('Success'));
 }
