049cb7205914ed671ff5e41e368b66fe68a19334
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-03 11:40:00 +1100

    test: 2-then results

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,4 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return Promise.any(promise).then(() => 'Success', () => new Error())
+  return Promise.any(promise)
+    .then(() => new Object('body', 'success', 'status', 200), () => new Error())
     .finally(console.log('Got a response from the API'));
 }
