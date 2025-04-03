a9e29c34e3592f5559c2a3c1b525e9f01f959e4f
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-03 14:34:07 +1100

    fix: 3-all.js access values

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,6 +2,5 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => console.log(values))
-    .finally(console.log('Success'));
+    .then((values) => console.log(`${values.body} ${values.firstName} ${values.lastName}`));
 }
