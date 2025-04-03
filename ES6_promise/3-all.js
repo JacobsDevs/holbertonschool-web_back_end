40c2c8a8c7666d384425f00e62a12c5f0febc880
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-03 14:42:34 +1100

    fix: 3-all.js error handling

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,7 +1,10 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => { console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`); })
-    .catch(console.log('Signup system offline'));
+  try {
+    return Promise.all([uploadPhoto(), createUser()])
+      .then((values) => { console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`); });
+  } catch (error) {
+    return console.log('Signup system offline');
+  }
 }
