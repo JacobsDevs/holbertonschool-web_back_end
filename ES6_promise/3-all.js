5980be36c85acce5d8470511a8ffddfa6251eab3
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-03 14:37:13 +1100

    fix: 3-all.js access correct elements

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,5 +2,5 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => { console.log(values); });
+    .then((values) => { console.log(`${values[0].body} ${values[1].firstName} ${values[2].lastName}`); });
 }
