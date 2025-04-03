95f1ae26c7d058c2c95ebf2c4cc094a54e997385
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-03 14:35:27 +1100

    fix: 3-all.js correct console.log syntax attempt 1

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,5 +2,5 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => console.log(`${values.body} ${values.firstName} ${values.lastName}`));
+    .then((values) => { console.log(`${values.body} ${values.firstName} ${values.lastName}`); });
 }
