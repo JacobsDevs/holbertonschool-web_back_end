26092626678072755ab072d3ccb3594a56ffc013
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-04 14:52:48 +1100

    fix: 6-final-user.js map values correctly

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,12 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      values.map((e) => {
-        if (e.status === 'fulfilled') {
-          return { status: e.status, value: e.value };
-        }
-        return { status: e.status, value: e.reason };
-      });
-    });
+    .then((values) => values.map((e) => {
+      if (e.status === 'fulfilled') {
+        return { status: e.status, value: e.value };
+      }
+      return { status: e.status, value: e.reason };
+    }));
 }
