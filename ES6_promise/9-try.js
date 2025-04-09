0fd05e2c24b3eb35349e3ecb76b9b6f4187668a3
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-09 13:15:57 +1000

    fix: 9-try.js promise issue

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,24 +1,18 @@
 export default async function guardrail(mathFunction) {
-  try {
-    const queue = [];
-    const response = await Promise.try(mathFunction, queue)
-      .then(
-        (value) => {
-          queue.push(value);
-        },
-      )
-      .catch(
-        (reason) => {
-          queue.push(reason.toString());
-        },
-      )
-      .finally(
-        () => {
-          queue.push('Guardrail was processed');
-        },
-      );
-    return queue;
-  } catch (err) {
-    console.log(err);
-  }
+  const queue = [];
+  const promise = await Promise.try(mathFunction, queue)
+    .then(
+      (value) => {
+        queue.push(value);
+      },
+      (reason) => {
+        queue.push(reason);
+      },
+    )
+    .finally(
+      () => {
+        queue.push('Guardrail was processed');
+      },
+    );
+  return queue;
 }
