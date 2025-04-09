e8536f5683f1f3bff40a2740fa4c5fd773abba81
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-09 13:04:52 +1000

    add: 9-try.js

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,21 +1,24 @@
 export default async function guardrail(mathFunction) {
-  const queue = [];
-  await Promise.try(mathFunction, queue)
-    .then(
-      (value) => {
-        queue.push(value);
-      },
-    )
-    .catch(
-      (reason) => {
-        queue.push(reason.toString());
-      },
-    )
-    .finally(
-      () => {
-        queue.push('Guardrail was processed');
-        console.log(queue);
-      },
-    );
-  return queue;
+  try {
+    const queue = [];
+    const response = await Promise.try(mathFunction, queue)
+      .then(
+        (value) => {
+          queue.push(value);
+        },
+      )
+      .catch(
+        (reason) => {
+          queue.push(reason.toString());
+        },
+      )
+      .finally(
+        () => {
+          queue.push('Guardrail was processed');
+        },
+      );
+    return queue;
+  } catch (err) {
+    console.log(err);
+  }
 }
