07e2ac2c9ec3a5003e19ccf20ffde3224193017d
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-29 10:52:40 +1000

    add: 1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+Concat 2 strings and return a string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatted string"""
+    return str1 + str2
