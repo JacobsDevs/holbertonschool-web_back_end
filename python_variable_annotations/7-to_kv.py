3351a965f85a00f911ed61f03c4d4b6cb3819e4a
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-29 11:29:04 +1000

    fix: 7-* use typing.Tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,6 +6,6 @@ Example of defining variables in types
 import typing
 
 
-def to_kv(k: str, v: typing.Union[int, float]) -> tuple:
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
     """Return a kv pair"""
-    return (k, float(v**2))
+    return (k, float(v ** 2))
