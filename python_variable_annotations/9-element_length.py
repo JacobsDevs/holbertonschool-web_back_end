aa328ff18337799f6a366a3d58a8c8dcc0c84402
JacobsDevs <jacobdevsmail@gmail.com>
2025-04-29 11:43:26 +1000

    fix: 9-element_length.py docstring

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -7,4 +7,5 @@ import typing
 
 
 def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+    """This is a function"""
     return [(i, len(i)) for i in lst]
