ef89f4bce7cadf66d0567d25cd75465818f6cfa4
JacobsDevs <jacobdevsmail@gmail.com>
2025-05-28 12:44:12 +1000

    fix: 2-read_file make syncronous

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,28 +1,23 @@
 const fs = require('node:fs');
 
 module.exports = function countStudents(path) {
-  const cs = [];
-  const swe = [];
-  const stream = fs.createReadStream(path, 'utf8');
-  stream.on('data', (row) => {
-    const rows = row.split('\n');
+  try {
+    const cs = [];
+    const swe = [];
+    const stream = fs.readFileSync(path, 'utf8');
+    const rows = stream.split('\n').slice(1);
     for (const i in rows) {
-      if (i !== '0') {
-        const row = rows[i].split(',');
-        if (row.length === 1) {
-          console.log(`Number of students: ${cs.length + swe.length}`);
-          console.log(`Number of students is CS: ${cs.length}. List: ${cs.join(', ')}`);
-          console.log(`Number of students is SWE: ${swe.length}. List: ${swe.join(', ')}`);
-        }
-        if (row[3] === 'CS') {
-          cs.push(row[0]);
-        } else {
-          swe.push(row[0]);
-        }
+      const row = rows[i].split(',');
+      if (row[3] === 'CS') {
+        cs.push(row[0]);
+      } if (row[3] === 'SWE') {
+        swe.push(row[0]);
       }
     }
-  });
-  stream.on('error', () => {
+    console.log(`Number of students: ${cs.length + swe.length}`);
+    console.log(`Number of students is CS: ${cs.length}. List: ${cs.join(', ')}`);
+    console.log(`Number of students is SWE: ${swe.length}. List: ${swe.join(', ')}`);
+  } catch (err) {
     throw new Error('Cannot load the database');
-  });
+  }
 };
