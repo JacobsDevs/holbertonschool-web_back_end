ab9b1838b369da5464263c2a1067ca2da9e15966
JacobsDevs <jacobdevsmail@gmail.com>
2025-05-28 11:10:01 +1000

    fix: 1-* remove all \n

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString()}`);
-  console.log('\n');
+  console.log(`Your name is: ${data.toString().replace('\n', '')}`);
   process.exit();
 });
 
