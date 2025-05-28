4dd47df0fb1481e4c5f9fc2aa9c383e128c76b48
JacobsDevs <jacobdevsmail@gmail.com>
2025-05-28 11:14:38 +1000

    fix: 1-* handle EOF

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data.toString()}`);
-  process.exit();
 });
 
 process.stdin.on('end', () => {
