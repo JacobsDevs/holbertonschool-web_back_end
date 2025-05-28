ced33315e579495e280b8308fc9e07b92354c748
JacobsDevs <jacobdevsmail@gmail.com>
2025-05-28 11:13:09 +1000

    fix: 1-* choose process.stdout.write

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString().replace('\n', '')}`);
+  process.stdout.write(`Your name is: ${data.toString()}`);
   process.exit();
 });
 
