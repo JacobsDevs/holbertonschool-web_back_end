78c228cf5e58be9a37e1c4b2f57957ed3cdf68db
JacobsDevs <jacobdevsmail@gmail.com>
2025-05-28 12:50:46 +1000

    fix: 2-read_file change is to in during console.log

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -4,19 +4,19 @@ module.exports = function countStudents(path) {
   try {
     const cs = [];
     const swe = [];
-    const stream = fs.readFileSync(path, 'utf8');
+    const stream = fs.readFileSync(path, 'utf-8');
     const rows = stream.split('\n').slice(1);
-    for (const i in rows) {
-      const row = rows[i].split(',');
-      if (row[3] === 'CS') {
-        cs.push(row[0]);
-      } if (row[3] === 'SWE') {
-        swe.push(row[0]);
+    for (const row of rows) {
+      const data = row.split(',');
+      if (data[3] === 'CS') {
+        cs.push(data[0]);
+      } if (data[3] === 'SWE') {
+        swe.push(data[0]);
       }
     }
     console.log(`Number of students: ${cs.length + swe.length}`);
-    console.log(`Number of students is CS: ${cs.length}. List: ${cs.join(', ')}`);
-    console.log(`Number of students is SWE: ${swe.length}. List: ${swe.join(', ')}`);
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
   } catch (err) {
     throw new Error('Cannot load the database');
   }
